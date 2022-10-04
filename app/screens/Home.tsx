068b71ff5583a0b12669b62b69f49b293aa01d27
Ayush <ayushagarwal.dev@gmail.com>
2022-10-04 23:52:23 +0530

    add react native vector icons

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 interface Props {}
 
@@ -8,13 +9,17 @@ const Home: FC<Props> = (): JSX.Element => {
     // Capture Button
     <View style={styles.container}>
       <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.button}></TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Icon name="camera" style={styles.icon} />
+        </TouchableOpacity>
         <Text style={styles.btnLabel}>Caputre</Text>
       </View>
 
       {/* Select Button */}
       <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.button}></TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Icon name="folder-open" style={styles.icon} />
+        </TouchableOpacity>
         <Text style={styles.btnLabel}>Select</Text>
       </View>
     </View>
@@ -36,9 +41,15 @@ const styles = StyleSheet.create({
     borderWidth: 4,
     borderColor: '#6C9ADE',
     borderRadius: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   btnLabel: {
     textAlign: 'center',
     fontWeight: '500',
+  },
+  icon: {
+    color: '#6C9ADE',
+    fontSize: 55,
   },
 });
