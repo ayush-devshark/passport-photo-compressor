76efda8798373955c953ab5380677a4bfa6dd2a2
Ayush <ayushagarwal.dev@gmail.com>
2022-10-07 23:02:05 +0530

    add image picker package and configure add code for user permissions

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,11 +1,42 @@
 import React, {FC} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, PermissionsAndroid, Alert} from 'react-native';
 import LargeIconButton from '../components/LargeIconButton';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 interface Props {}
 
 const Home: FC<Props> = (): JSX.Element => {
+  const handleImageCapture = async (): Promise<void> => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: 'Camera Permission',
+          message: 'App needs access to your camera so you can take pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      const {NEVER_ASK_AGAIN, DENIED} = PermissionsAndroid.RESULTS;
+
+      if (granted === NEVER_ASK_AGAIN) {
+        return Alert.alert(
+          'Fail to open camera',
+          'Its looks like camera permissions are not granted. Please allow access for proper use of app!',
+        );
+      }
+      if (granted === DENIED) {
+        return Alert.alert(
+          'Fail to open camera',
+          'Sorry to use this feature, camera permissions are required.',
+        );
+      }
+    } catch (err) {
+      console.log('Fail to open camera error!', err);
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* App Titles */}
@@ -18,7 +49,7 @@ const Home: FC<Props> = (): JSX.Element => {
       </View>
 
       {/* // Capture Button */}
-      <LargeIconButton title="Capture">
+      <LargeIconButton title="Capture" onPress={handleImageCapture}>
         <Icon name="camera" />
       </LargeIconButton>
 
