867bfb9146817166b2f729880f66dc3fc8ce9e87
Ayush <ayushagarwal.dev@gmail.com>
2022-10-09 00:17:27 +0530

    read route params

--- app/screens/ImageEditor.tsx
+++ app/screens/ImageEditor.tsx
@@ -1,9 +1,16 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React, {FC} from 'react';
 import {StyleSheet, View} from 'react-native';
+import {RootStackParamList} from '../navigation/AppNavigator';
 
-interface Props {}
+type RouteProps = StackScreenProps<RootStackParamList, 'ImageEditor'>;
+interface Props {
+  route: RouteProps['route'];
+}
 
-const ImageEditor: FC<Props> = (): JSX.Element => {
+const ImageEditor: FC<Props> = ({route}): JSX.Element => {
+  console.log(route.params.imageUri);
+
   return <View style={styles.container}></View>;
 };
 
